22629fc8d489e5a087262f0bdced00b59aa50af4
Taha Jumaah <taharasheed95@gmail.com>
2024-02-29 11:55:21 +0100

    updated viteconfig again

--- src/api/checkSolution.js
+++ src/api/checkSolution.js
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 
 export default async function checkSolution(Event, params, setIsFound) {
   const device = "mobile";
+  const server = import.meta.env.VITE_server;
   try {
     const imgH = Event.target.height;
     const imgW = Event.target.width;
@@ -9,17 +10,14 @@ export default async function checkSolution(Event, params, setIsFound) {
     const offsetY = Event.nativeEvent.offsetY;
     const percentX = (offsetX / imgW) * 100;
     const percentY = (offsetY / imgH) * 100;
-    const response = await fetch(
-      `http://127.0.0.1:3000/api/${params.difficulty}/check`,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-        body: JSON.stringify({ solX: percentX, solY: percentY, device }),
-        credentials: "include",
-      }
-    );
+    const response = await fetch(`${server}/api/${params.difficulty}/check`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ solX: percentX, solY: percentY, device }),
+      credentials: "include",
+    });
     const resData = await response.json();
     setIsFound(resData.waldoFound);
   } catch (error) {
